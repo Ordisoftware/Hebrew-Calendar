78b75b3d66d7d977bb043fae7df5285dbdb14920
Olivier Rogier <github.com@ordisoftware.org>
2019-01-15 23:33:31 +0100

    Update README.md

--- Project/Properties/Settings.Designer.cs
+++ Project/Properties/Settings.Designer.cs
@@ -181,7 +181,7 @@ namespace Ordisoftware.HebrewCalendar.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("48.52")]
+        [global::System.Configuration.DefaultSettingValueAttribute("48.85")]
         public float Latitude {
             get {
                 return ((float)(this["Latitude"]));
@@ -193,7 +193,7 @@ namespace Ordisoftware.HebrewCalendar.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.15")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.35")]
         public float Longitude {
             get {
                 return ((float)(this["Longitude"]));
