01540e4db2af3275ae0c45ae33dd9c8859daaca1
Olivier Rogier <github.com@ordisoftware.org>
2019-10-17 16:57:09 +0200

    Improve setup

---
+++ Setup/SQLiteODBCInstaller/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SQLiteODBCInstaller
+{
+
+  static class Program
+  {
+
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      try
+      {
+        using ( RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\ODBC\\ODBC.INI\\SQLite3 Datasource") )
+          if ( key != null )
+          {
+            var o = key.GetValue("Driver");
+            if ( o != null )
+              if ( (string)o == "C:\\Windows\\system32\\sqlite3odbc.dll" )
+                return;
+          }
+      }
+      catch ( Exception ex )
+      {
+        MessageBox.Show(ex.Message);
+      }
+      if ( IntPtr.Size == 8 )
+        RunShell("sqliteodbc_w64.exe");
+      else
+        RunShell("sqliteodbc_w32.exe");
+    }
+
+    static public void RunShell(string filename)
+    {
+      using ( var process = new Process() )
+        try
+        {
+          process.StartInfo.FileName = filename;
+          process.Start();
+        }
+        catch ( Exception ex )
+        {
+          MessageBox.Show(ex.Message + Environment.NewLine + process.StartInfo.FileName);
+        }
+    }
+
+  }
+
+}
