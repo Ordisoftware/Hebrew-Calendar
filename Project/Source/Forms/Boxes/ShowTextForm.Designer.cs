d1597b30816aa933318d96028c01b53ab3bd270b
Olivier Rogier <github.com@ordisoftware.org>
2020-04-08 17:23:21 +0200

    Add show text form

---
+++ Project/Source/Forms/Boxes/ShowTextForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Ordisoftware.HebrewCalendar
+{
+  partial class ShowTextForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if ( disposing && ( components != null ) )
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowTextForm));
+      this.PanelBottom = new System.Windows.Forms.Panel();
+      this.ActionClose = new System.Windows.Forms.Button();
+      this.TextBox = new System.Windows.Forms.TextBox();
+      this.Panel = new System.Windows.Forms.Panel();
+      this.PanelBottom.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // PanelBottom
+      // 
+      this.PanelBottom.Controls.Add(this.ActionClose);
+      resources.ApplyResources(this.PanelBottom, "PanelBottom");
+      this.PanelBottom.Name = "PanelBottom";
+      // 
+      // ActionClose
+      // 
+      resources.ApplyResources(this.ActionClose, "ActionClose");
+      this.ActionClose.DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.ActionClose.Name = "ActionClose";
+      // 
+      // TextBox
+      // 
+      this.TextBox.BackColor = System.Drawing.SystemColors.Window;
+      resources.ApplyResources(this.TextBox, "TextBox");
+      this.TextBox.Name = "TextBox";
+      this.TextBox.ReadOnly = true;
+      this.TextBox.TabStop = false;
+      // 
+      // Panel
+      // 
+      resources.ApplyResources(this.Panel, "Panel");
+      this.Panel.Name = "Panel";
+      // 
+      // ShowTextForm
+      // 
+      this.AcceptButton = this.ActionClose;
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.ActionClose;
+      this.Controls.Add(this.TextBox);
+      this.Controls.Add(this.Panel);
+      this.Controls.Add(this.PanelBottom);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.MaximizeBox = false;
+      this.MinimizeBox = false;
+      this.Name = "ShowTextForm";
+      this.ShowInTaskbar = false;
+      this.PanelBottom.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Panel PanelBottom;
+    private System.Windows.Forms.Button ActionClose;
+    private System.Windows.Forms.TextBox TextBox;
+    private System.Windows.Forms.Panel Panel;
+  }
+}
---
+++ Project/Source/Forms/Boxes/ShowTextForm.cs
@@ -0,0 +1,51 @@
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Calendar.
+/// Copyright 2016-2020 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at 
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file, 
+/// then You may include the notice in a location(such as a LICENSE file in a 
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2020-04 </created>
+/// <edited> 2020-04 </edited>
+using System;
+using System.Windows.Forms;
+
+namespace Ordisoftware.HebrewCalendar
+{
+  public partial class ShowTextForm : Form
+  {
+
+    static public void RunCelebrationsNotice()
+    {
+      Run(MainForm.Instance.ActionShowCelebrationsNotice.Text, Translations.CelebrationsNotice.GetLang(), 500, 325);
+    }
+
+    static public void RunShabatNotice()
+    {
+      Run(MainForm.Instance.ActionShowShabatNotice.Text, Translations.PersonalShabatNotice.GetLang(), 625, 500);
+    }
+
+    static public void Run(string title, string str, int width, int height)
+    {
+      var form = new ShowTextForm();
+      form.Text = title;
+      form.TextBox.Text = str;
+      form.Width = width;
+      form.Height = height;
+      form.ShowDialog();
+    }
+
+    private ShowTextForm()
+    {
+      InitializeComponent();
+      Icon = MainForm.Instance.Icon;
+    }
+
+  }
+
+}
