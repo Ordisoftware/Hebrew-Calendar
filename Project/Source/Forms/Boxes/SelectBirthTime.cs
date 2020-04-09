239b5dd1446b922e1f3237ba3460979972b8de75
Olivier Rogier <github.com@ordisoftware.org>
2020-04-09 11:38:17 +0200

    Add file header

--- Project/Source/Forms/Boxes/SelectBirthTime.cs
+++ Project/Source/Forms/Boxes/SelectBirthTime.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Calendar.
+/// Copyright 2016-2020 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at 
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file, 
+/// then You may include the notice in a location(such as a LICENSE file in a 
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2019-10 </created>
+/// <edited> 2019-10 </edited>
+using System;
 using System.Windows.Forms;
 
 namespace Ordisoftware.HebrewCalendar
@@ -23,6 +30,7 @@ namespace Ordisoftware.HebrewCalendar
     {
       DialogResult = DialogResult.OK;
     }
+
   }
 
 }
