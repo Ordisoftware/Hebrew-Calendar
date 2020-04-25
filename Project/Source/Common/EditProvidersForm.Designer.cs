b4e57f99eab56545444e9f44ad0b2e44321bd24c
Olivier Rogier <github.com@ordisoftware.org>
2020-04-25 15:33:49 +0200

    Improve UI

--- Project/Source/Common/EditProvidersForm.Designer.cs
+++ Project/Source/Common/EditProvidersForm.Designer.cs
@@ -28,6 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EditProvidersForm));
       this.PanelBottomSeparator = new System.Windows.Forms.Panel();
       this.PanelBottom = new System.Windows.Forms.Panel();
       this.ActionOk = new System.Windows.Forms.Button();
@@ -38,71 +39,46 @@
       // 
       // PanelBottomSeparator
       // 
-      this.PanelBottomSeparator.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.PanelBottomSeparator.Location = new System.Drawing.Point(10, 518);
+      resources.ApplyResources(this.PanelBottomSeparator, "PanelBottomSeparator");
       this.PanelBottomSeparator.Name = "PanelBottomSeparator";
-      this.PanelBottomSeparator.Size = new System.Drawing.Size(772, 10);
-      this.PanelBottomSeparator.TabIndex = 42;
       // 
       // PanelBottom
       // 
+      resources.ApplyResources(this.PanelBottom, "PanelBottom");
       this.PanelBottom.Controls.Add(this.ActionOk);
       this.PanelBottom.Controls.Add(this.ActionCancel);
-      this.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-      this.PanelBottom.Location = new System.Drawing.Point(10, 528);
       this.PanelBottom.Name = "PanelBottom";
-      this.PanelBottom.Size = new System.Drawing.Size(772, 28);
-      this.PanelBottom.TabIndex = 41;
       // 
       // ActionOk
       // 
-      this.ActionOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      resources.ApplyResources(this.ActionOk, "ActionOk");
       this.ActionOk.DialogResult = System.Windows.Forms.DialogResult.OK;
-      this.ActionOk.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-      this.ActionOk.Location = new System.Drawing.Point(613, 2);
       this.ActionOk.Name = "ActionOk";
-      this.ActionOk.Size = new System.Drawing.Size(75, 24);
-      this.ActionOk.TabIndex = 0;
-      this.ActionOk.Text = "OK";
       this.ActionOk.Click += new System.EventHandler(this.ActionOk_Click);
       // 
       // ActionCancel
       // 
-      this.ActionCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      resources.ApplyResources(this.ActionCancel, "ActionCancel");
       this.ActionCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-      this.ActionCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-      this.ActionCancel.Location = new System.Drawing.Point(694, 2);
       this.ActionCancel.Name = "ActionCancel";
-      this.ActionCancel.Size = new System.Drawing.Size(75, 24);
-      this.ActionCancel.TabIndex = 1;
-      this.ActionCancel.Text = "Cancel";
       // 
       // TabControl
       // 
-      this.TabControl.Appearance = System.Windows.Forms.TabAppearance.Buttons;
-      this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-      this.TabControl.Location = new System.Drawing.Point(10, 10);
+      resources.ApplyResources(this.TabControl, "TabControl");
       this.TabControl.Multiline = true;
       this.TabControl.Name = "TabControl";
       this.TabControl.SelectedIndex = 0;
-      this.TabControl.Size = new System.Drawing.Size(772, 508);
-      this.TabControl.TabIndex = 0;
       // 
       // EditProvidersForm
       // 
       this.AcceptButton = this.ActionOk;
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      resources.ApplyResources(this, "$this");
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.CancelButton = this.ActionCancel;
-      this.ClientSize = new System.Drawing.Size(792, 566);
       this.Controls.Add(this.TabControl);
       this.Controls.Add(this.PanelBottomSeparator);
       this.Controls.Add(this.PanelBottom);
-      this.Location = new System.Drawing.Point(-1, -1);
-      this.MinimumSize = new System.Drawing.Size(600, 500);
       this.Name = "EditProvidersForm";
-      this.Padding = new System.Windows.Forms.Padding(10);
-      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
       this.Load += new System.EventHandler(this.EditProvidersForm_Load);
       this.PanelBottom.ResumeLayout(false);
       this.ResumeLayout(false);
