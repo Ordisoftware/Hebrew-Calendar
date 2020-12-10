4a7b0a925d5ac2126975959b312dc088bb4aa57f
Olivier Rogier <github.com@ordisoftware.org>
2020-12-10 18:41:54 +0100

    Update dependency

--- Project/Dependencies/AASharp/Src/AASharp/AAS3DCoordinate.cs
+++
@@ -1,9 +0,0 @@
-namespace AASharp
-{
-    public class AAS3DCoordinate
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Z { get; set; }
-    }
-}
