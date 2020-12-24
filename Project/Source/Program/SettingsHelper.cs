e2fc535af426305d42831355e099eea9a57fd27e
Olivier Rogier <github.com@ordisoftware.org>
2020-12-24 12:10:02 +0100

    Refactor

--- Project/Source/Program/SettingsHelper.cs
+++ Project/Source/Program/SettingsHelper.cs
@@ -26,6 +26,8 @@ namespace Ordisoftware.Hebrew.Calendar
   static internal class SettingsHelper
   {
 
+    static private bool Mutex;
+
     /// <summary>
     /// Indicate the main form instance.
     /// </summary>
@@ -70,43 +72,52 @@ namespace Ordisoftware.Hebrew.Calendar
     /// <param name="settings">The settings to act on.</param>
     static internal void Retrieve(this Settings settings)
     {
-      var area = SystemInformation.WorkingArea;
-      if ( settings.MainFormLeft >= area.Left && settings.MainFormLeft <= area.Width )
-        MainForm.Left = settings.MainFormLeft;
-      else
-        MainForm.Left = area.Left;
-      if ( settings.MainFormTop >= area.Top && settings.MainFormTop <= area.Height )
-        MainForm.Top = settings.MainFormTop;
-      else
-        MainForm.Top = area.Top;
-      if ( settings.MainFormWidth >= MainForm.MinimumSize.Width && settings.MainFormWidth <= area.Width )
-        MainForm.Width = settings.MainFormWidth;
-      else
-        MainForm.Width = MainForm.MinimumSize.Width;
-      if ( settings.MainFormHeight >= MainForm.MinimumSize.Height && settings.MainFormHeight <= area.Height )
-        MainForm.Height = settings.MainFormHeight;
-      else
-        MainForm.Height = MainForm.MinimumSize.Height;
-      MainForm.EditScreenNone.Checked = settings.MainFormPosition == ControlLocation.Loose;
-      MainForm.EditScreenTopLeft.Checked = settings.MainFormPosition == ControlLocation.TopLeft;
-      MainForm.EditScreenTopRight.Checked = settings.MainFormPosition == ControlLocation.TopRight;
-      MainForm.EditScreenBottomLeft.Checked = settings.MainFormPosition == ControlLocation.BottomLeft;
-      MainForm.EditScreenBottomRight.Checked = settings.MainFormPosition == ControlLocation.BottomRight;
-      MainForm.EditScreenCenter.Checked = settings.MainFormPosition == ControlLocation.Center;
-      MainForm.EditScreenPosition_Click(null, null);
-      MainForm.WindowState = settings.MainFormState;
-      //
-      MainForm.EditConfirmClosing.Checked = settings.ConfirmClosing;
-      MainForm.EditShowTips.Checked = settings.ShowTips;
-      MainForm.EditESCtoExit.Checked = settings.ESCtoExit;
-      MainForm.EditSoundsEnabled.Checked = settings.SoundsEnabled;
-      MainForm.EditUseAdvancedDialogBoxes.Checked = settings.AdvancedDialogBoxes;
-      MainForm.EditShowSuccessDialogs.Checked = settings.ShowSuccessDialogs;
-      DisplayManager.ShowSuccessDialogs = settings.ShowSuccessDialogs;
-      MainForm.EditDialogBoxesSettings_CheckedChanged(null, null);
-      //
-      if ( settings.AutoOpenExportedFile && settings.AutoOpenExportFolder )
-        settings.AutoOpenExportFolder = false;
+      if ( Mutex ) return;
+      Mutex = true;
+      try
+      {
+        var area = SystemInformation.WorkingArea;
+        if ( settings.MainFormLeft >= area.Left && settings.MainFormLeft <= area.Width )
+          MainForm.Left = settings.MainFormLeft;
+        else
+          MainForm.Left = area.Left;
+        if ( settings.MainFormTop >= area.Top && settings.MainFormTop <= area.Height )
+          MainForm.Top = settings.MainFormTop;
+        else
+          MainForm.Top = area.Top;
+        if ( settings.MainFormWidth >= MainForm.MinimumSize.Width && settings.MainFormWidth <= area.Width )
+          MainForm.Width = settings.MainFormWidth;
+        else
+          MainForm.Width = MainForm.MinimumSize.Width;
+        if ( settings.MainFormHeight >= MainForm.MinimumSize.Height && settings.MainFormHeight <= area.Height )
+          MainForm.Height = settings.MainFormHeight;
+        else
+          MainForm.Height = MainForm.MinimumSize.Height;
+        MainForm.EditScreenNone.Checked = settings.MainFormPosition == ControlLocation.Loose;
+        MainForm.EditScreenTopLeft.Checked = settings.MainFormPosition == ControlLocation.TopLeft;
+        MainForm.EditScreenTopRight.Checked = settings.MainFormPosition == ControlLocation.TopRight;
+        MainForm.EditScreenBottomLeft.Checked = settings.MainFormPosition == ControlLocation.BottomLeft;
+        MainForm.EditScreenBottomRight.Checked = settings.MainFormPosition == ControlLocation.BottomRight;
+        MainForm.EditScreenCenter.Checked = settings.MainFormPosition == ControlLocation.Center;
+        MainForm.EditScreenPosition_Click(null, null);
+        MainForm.WindowState = settings.MainFormState;
+        //
+        MainForm.EditConfirmClosing.Checked = settings.ConfirmClosing;
+        MainForm.EditShowTips.Checked = settings.ShowTips;
+        MainForm.EditESCtoExit.Checked = settings.ESCtoExit;
+        MainForm.EditSoundsEnabled.Checked = settings.SoundsEnabled;
+        MainForm.EditUseAdvancedDialogBoxes.Checked = settings.AdvancedDialogBoxes;
+        MainForm.EditShowSuccessDialogs.Checked = settings.ShowSuccessDialogs;
+        DisplayManager.ShowSuccessDialogs = settings.ShowSuccessDialogs;
+        MainForm.EditDialogBoxesSettings_CheckedChanged(null, null);
+        //
+        if ( settings.AutoOpenExportedFile && settings.AutoOpenExportFolder )
+          settings.AutoOpenExportFolder = false;
+      }
+      finally
+      {
+        Mutex = false;
+      }
     }
 
     /// <summary>
@@ -115,30 +126,39 @@ namespace Ordisoftware.Hebrew.Calendar
     /// <param name="settings">The settings to act on.</param>
     static internal void Store(this Settings settings)
     {
-      var winState = MainForm.WindowState;
-      if ( winState != FormWindowState.Minimized )
-        settings.MainFormState = winState;
-      if ( winState == FormWindowState.Normal )
+      if ( Mutex ) return;
+      Mutex = true;
+      try
       {
-        settings.MainFormLeft = MainForm.Left;
-        settings.MainFormTop = MainForm.Top;
-        settings.MainFormWidth = MainForm.Width;
-        settings.MainFormHeight = MainForm.Height;
+        var winState = MainForm.WindowState;
+        if ( winState != FormWindowState.Minimized )
+          settings.MainFormState = winState;
+        if ( winState == FormWindowState.Normal )
+        {
+          settings.MainFormLeft = MainForm.Left;
+          settings.MainFormTop = MainForm.Top;
+          settings.MainFormWidth = MainForm.Width;
+          settings.MainFormHeight = MainForm.Height;
+        }
+        if ( MainForm.EditScreenNone.Checked ) settings.MainFormPosition = ControlLocation.Loose;
+        if ( MainForm.EditScreenTopLeft.Checked ) settings.MainFormPosition = ControlLocation.TopLeft;
+        if ( MainForm.EditScreenTopRight.Checked ) settings.MainFormPosition = ControlLocation.TopRight;
+        if ( MainForm.EditScreenBottomLeft.Checked ) settings.MainFormPosition = ControlLocation.BottomLeft;
+        if ( MainForm.EditScreenBottomRight.Checked ) settings.MainFormPosition = ControlLocation.BottomRight;
+        if ( MainForm.EditScreenCenter.Checked ) settings.MainFormPosition = ControlLocation.Center;
+        //
+        settings.ConfirmClosing = MainForm.EditConfirmClosing.Checked;
+        settings.ShowTips = MainForm.EditShowTips.Checked;
+        settings.ESCtoExit = MainForm.EditESCtoExit.Checked;
+        settings.SoundsEnabled = MainForm.EditSoundsEnabled.Checked;
+        settings.AdvancedDialogBoxes = MainForm.EditUseAdvancedDialogBoxes.Checked;
+        settings.ShowSuccessDialogs = MainForm.EditShowSuccessDialogs.Checked;
+        settings.Save();
       }
-      if ( MainForm.EditScreenNone.Checked ) settings.MainFormPosition = ControlLocation.Loose;
-      if ( MainForm.EditScreenTopLeft.Checked ) settings.MainFormPosition = ControlLocation.TopLeft;
-      if ( MainForm.EditScreenTopRight.Checked ) settings.MainFormPosition = ControlLocation.TopRight;
-      if ( MainForm.EditScreenBottomLeft.Checked ) settings.MainFormPosition = ControlLocation.BottomLeft;
-      if ( MainForm.EditScreenBottomRight.Checked ) settings.MainFormPosition = ControlLocation.BottomRight;
-      if ( MainForm.EditScreenCenter.Checked ) settings.MainFormPosition = ControlLocation.Center;
-      //
-      settings.ConfirmClosing = MainForm.EditConfirmClosing.Checked;
-      settings.ShowTips = MainForm.EditShowTips.Checked;
-      settings.ESCtoExit = MainForm.EditESCtoExit.Checked;
-      settings.SoundsEnabled = MainForm.EditSoundsEnabled.Checked;
-      settings.AdvancedDialogBoxes = MainForm.EditUseAdvancedDialogBoxes.Checked;
-      settings.ShowSuccessDialogs = MainForm.EditShowSuccessDialogs.Checked;
-      settings.Save();
+      finally
+      {
+        Mutex = false;
+      }
     }
 
     /// <summary>
