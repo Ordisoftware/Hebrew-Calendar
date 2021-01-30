2deacb8c4dcc313de3b373c00ad961dd181961aa
Olivier Rogier <github.com@ordisoftware.org>
2021-01-30 21:06:10 +0100

    Fix settings

--- Project/Source/Forms/Config/PreferencesForm.cs
+++ Project/Source/Forms/Config/PreferencesForm.cs
@@ -214,6 +214,7 @@ namespace Ordisoftware.Hebrew.Calendar
 
     private void EditVolume_ValueChanged(object sender, EventArgs e)
     {
+      if ( !IsReady ) return;
       MediaMixer.SetApplicationVolume(System.Diagnostics.Process.GetCurrentProcess().Id, EditVolume.Value);
       LabelVolumeValue.Text = EditVolume.Value + "%";
       Program.Settings.ApplicationVolume = EditVolume.Value;
