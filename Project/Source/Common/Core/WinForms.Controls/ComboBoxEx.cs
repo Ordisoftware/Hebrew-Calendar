daf1da3e7e54f9d764daf50002e8785d16ac636e
Olivier Rogier <github.com@ordisoftware.org>
2021-03-23 07:05:47 +0100

    Update header file date

--- Project/Source/Common/Core/WinForms.Controls/ComboBoxEx.cs
+++ Project/Source/Common/Core/WinForms.Controls/ComboBoxEx.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2021-02 </created>
-/// <edited> 2021-02 </edited>
+/// <edited> 2021-03 </edited>
 using System;
 using System.Windows.Forms;
 
--- Project/Source/Common/Hebrew/WinForms/LettersControl.Redraw.cs
+++ Project/Source/Common/Hebrew/WinForms/LettersControl.Redraw.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2012-10 </created>
-/// <edited> 2020-08 </edited>
+/// <edited> 2021-03 </edited>
 using System;
 using System.Drawing;
 using System.Windows.Forms;
