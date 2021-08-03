8f62682148e021f366117fbac572854992e9b57a
Olivier Rogier <github.com@ordisoftware.org>
2021-08-03 21:29:06 +0200

    Improve issue text

--- Project/Source/Common/Core/WinForms/ExceptionForm.cs
+++ Project/Source/Common/Core/WinForms/ExceptionForm.cs
@@ -11,7 +11,7 @@
 /// You may add additional accurate notices of copyright ownership.
 /// </license>
 /// <created> 2007-05 </created>
-/// <edited> 2021-04 </edited>
+/// <edited> 2021-07 </edited>
 using System;
 using System.Linq;
 using System.Net;
