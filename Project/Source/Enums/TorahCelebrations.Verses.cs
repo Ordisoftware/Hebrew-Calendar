df82ffb3f6c3c8386a6e7cb3de75f1877bf0cd48
Olivier Rogier <github.com@ordisoftware.org>
2021-09-13 08:58:18 +0200

    Add convocations verses

--- Project/Source/Enums/TorahCelebrations.Verses.cs
+++ Project/Source/Enums/TorahCelebrations.Verses.cs
@@ -28,88 +28,90 @@ namespace Ordisoftware.Hebrew.Calendar
   {
 
     /*
-        Shémot 5.1
-        Shémot 13.4
-        Shémot 12.1 - 12.51
-        Vayiqra 23.5 - 23.8
-        Bamidbar 9.1 - 9.14
-        Bamidbar 28.16 - 28.25
-        Bamidbar 33.3 - 33.4
-        Dévarim 16.1 - 16.9
 
-        Pessah
-        Exode 12.1 - 12.13
-        Exode 12.14 - 12.20
-        Exode 13.4
-        Exode 23.15 - 23.16
-        Lévitique 23.5 - 23.6
-        Lévitique 23.10 - 23.16
-        Deutéronome 16.1
-        Deutéronome 16.3
-        Deutéronome 16.2 - 16.8
-        Nombres 9.1 - 9.12
-        Nombres 28.16 - 28.17
-        Nombres 28.26
-        Josué 5.10
-        Rois II 23.21 - 23.23
-        Ezra 6.19 - 6.22
-        Chroniques II 8.12 - 8.13
-        Chroniques II 30.1 - 30.27
-        Chroniques II 35.1 - 35.19
+    Shémot 5.1
+    Shémot 13.4
+    Shémot 12.1 - 12.51
+    Vayiqra 23.5 - 23.8
+    Bamidbar 9.1 - 9.14
+    Bamidbar 28.16 - 28.25
+    Bamidbar 33.3 - 33.4
+    Dévarim 16.1 - 16.9
 
-        Chavouot
-        Exode 23.16 - 23.17
-        Exode 34.22
-        Exode 34.26
-        Lévitique 23.9 - 23.22
-        Deutéronome 16.1 - 16.12
-        Nombres 28.26 - 38.31
-        Rois II 4.42
-        Isaïe 9.2
-        Jérémie 5.24
-        Tobie 2.1 - 2.6
-        Macchabées II 12.29 - 12.32
-        Chroniques II 8.12 - 8.13
 
-        YomTerouah
-        Exode 12.2
-        Lévitique 23.24 - 23.25
-        Nombres 29.1 - 29.6
-        Néhémie 8.1 - 8.10
+    Pessah
+    Exode 12.1 - 12.13
+    Exode 12.14 - 12.20
+    Exode 13.4
+    Exode 23.15 - 23.16
+    Lévitique 23.5 - 23.6
+    Lévitique 23.10 - 23.16
+    Deutéronome 16.1
+    Deutéronome 16.3
+    Deutéronome 16.2 - 16.8
+    Nombres 9.1 - 9.12
+    Nombres 28.16 - 28.17
+    Nombres 28.26
+    Josué 5.10
+    Rois II 23.21 - 23.23
+    Ezra 6.19 - 6.22
+    Chroniques II 8.12 - 8.13
+    Chroniques II 30.1 - 30.27
+    Chroniques II 35.1 - 35.19
 
-        YomHaKipourim
-        Exode 30.1 - 30.10
-        Lévitique 10.1 - 10.3
-        Lévitique 16.1 – 16.34
-        Lévitique 23.26 – 23.32
-        Lévitique 25.9 - 25.10
-        Nombres 29.7 - 29.11
-        Isaïe 58.1 - 58.8
-        Zacharie 7.1 - 7.5 ?
-        Ézéchiel 40.1 ?
-        Ézéchiel 45.18 - 45.20 ?
+    Chavouot
+    Exode 23.16 - 23.17
+    Exode 34.22
+    Exode 34.26
+    Lévitique 23.9 - 23.22
+    Deutéronome 16.1 - 16.12
+    Nombres 28.26 - 38.31
+    Rois II 4.42
+    Isaïe 9.2
+    Jérémie 5.24
+    Tobie 2.1 - 2.6
+    Macchabées II 12.29 - 12.32
+    Chroniques II 8.12 - 8.13
 
-        Soukot
-        Exode 23.16 - 23.17
-        Exode 34.22
-        Lévitique 23.34 - 23.43
-        Deutéronome 16.13 - 16.16
-        Deutéronome 31.10 - 31.13
-        Nombres 29.12 - 29.39
-        Ézéchiel 45.25
-        Ezra 3.1 - 3.4
-        Isaïe 12.3
-        Juges 9.27
-        Juges 21.19
-        Néhémie 8.13 - 8.18
-        Rois I 8 
-        Rois I 8.2
-        Rois I 12.32
-        Rois I 65
-        Zacharie 14.16 - 14.19
-        Chroniques II 5.3
-        Chroniques II 7
-        Chroniques II 7.8
+    YomTerouah
+    Exode 12.2
+    Lévitique 23.24 - 23.25
+    Nombres 29.1 - 29.6
+    Néhémie 8.1 - 8.10
+
+    YomHaKipourim
+    Exode 30.1 - 30.10
+    Lévitique 10.1 - 10.3
+    Lévitique 16.1 – 16.34
+    Lévitique 23.26 – 23.32
+    Lévitique 25.9 - 25.10
+    Nombres 29.7 - 29.11
+    Isaïe 58.1 - 58.8
+    Zacharie 7.1 - 7.5 ?
+    Ézéchiel 40.1 ?
+    Ézéchiel 45.18 - 45.20 ?
+
+    Soukot
+    Exode 23.16 - 23.17
+    Exode 34.22
+    Lévitique 23.34 - 23.43
+    Deutéronome 16.13 - 16.16
+    Deutéronome 31.10 - 31.13
+    Nombres 29.12 - 29.39
+    Ézéchiel 45.25
+    Ezra 3.1 - 3.4
+    Isaïe 12.3
+    Juges 9.27
+    Juges 21.19
+    Néhémie 8.13 - 8.18
+    Rois I 8 
+    Rois I 8.2
+    Rois I 12.32
+    Rois I 65
+    Zacharie 14.16 - 14.19
+    Chroniques II 5.3
+    Chroniques II 7
+    Chroniques II 7.8 - 7.9
 
     */
 
