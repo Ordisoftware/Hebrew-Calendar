12f906c63a6f09997c49a0f70d2d62eeb1ee8958
Olivier Rogier <github.com@ordisoftware.org>
2021-11-15 13:40:45 +0100

    Refactor

---
+++ Project/Source/Enums/MoonriseOccurring.cs
@@ -0,0 +1,43 @@
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Calendar.
+/// Copyright 2016-2021 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file,
+/// then You may include the notice in a location(such as a LICENSE file in a
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2016-04 </created>
+/// <edited> 2016-04 </edited>
+using System;
+
+namespace Ordisoftware.Hebrew.Calendar
+{
+
+  /// <summary>
+  /// Moon rise occurring enum.
+  /// </summary>
+  public enum MoonriseOccurring
+  {
+
+    /// <summary>
+    /// Moonrise is after moonset.
+    /// </summary>
+    AfterSet,
+
+    /// <summary>
+    /// Moonrise is before moonset.
+    /// </summary>
+    BeforeSet,
+
+    /// <summary>
+    /// Moonrise is on next day.
+    /// </summary>
+    NextDay
+
+  }
+
+}
