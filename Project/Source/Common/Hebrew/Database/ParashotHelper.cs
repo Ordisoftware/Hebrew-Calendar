5209689175da1d84c2a80a17d44af7645ef30f7a
Olivier Rogier <github.com@ordisoftware.org>
2023-04-24 18:06:24 +0200

    Update file

--- Project/Source/Common/Hebrew/Database/ParashotHelper.cs
+++ Project/Source/Common/Hebrew/Database/ParashotHelper.cs
@@ -25,6 +25,7 @@ static public class ParashotHelper
 
   [SuppressMessage("Usage", "VSTHRD101:Avoid unsupported async delegates", Justification = "<En attente>")]
   [SuppressMessage("Design", "MA0051:Method is too long", Justification = "N/A")]
+  [SuppressMessage("Usage", "VSTHRD100:Avoid async void methods", Justification = "<En attente>")]
   static public bool ShowDescription(this List<Parashah> parashot,
                                      Parashah parashah,
                                      bool withLinked,
