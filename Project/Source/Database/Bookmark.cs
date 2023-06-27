247d3415ed4f99d93938c5b4bb925aa3075c6ccd
Olivier Rogier <github.com@ordisoftware.org>
2023-06-27 13:18:10 +0200

    Add file

---
+++ Project/Source/Database/Bookmark.cs
@@ -0,0 +1,27 @@
+﻿/// <license>
+/// This file is part of Ordisoftware Hebrew Calendar/Letters/Words.
+/// Copyright 2012-2023 Olivier Rogier.
+/// See www.ordisoftware.com for more information.
+/// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+/// If a copy of the MPL was not distributed with this file, You can obtain one at
+/// https://mozilla.org/MPL/2.0/.
+/// If it is not possible or desirable to put the notice in a particular file,
+/// then You may include the notice in a location(such as a LICENSE file in a
+/// relevant directory) where a recipient would be likely to look for such a notice.
+/// You may add additional accurate notices of copyright ownership.
+/// </license>
+/// <created> 2023-06 </created>
+/// <edited> 2023-06 </edited>
+namespace Ordisoftware.Hebrew;
+
+using SQLite;
+
+[Serializable]
+[Table("Bookmark")]
+public class Bookmark
+{
+  [PrimaryKey]
+  public string ID { get; set; }
+  public string Date { get; set; }
+  public string Memo { get; set; }
+}
