f8ae8feae9414e68b7a8fd4d3fdbdf956ed43cda
Olivier Rogier <github.com@ordisoftware.org>
2026-03-22 10:22:10 +0100

    Update file header

--- Project/Source/Common/Hebrew/Providers/Parashot/OnlineParashot.TheTorahCom.cs
+++ Project/Source/Common/Hebrew/Providers/Parashot/OnlineParashot.TheTorahCom.cs
@@ -1,6 +1,6 @@
 ﻿/// <license>
 /// This file is part of Ordisoftware Hebrew Calendar/Letters/Words/Pi.
-/// Copyright 2012-2025 Olivier Rogier.
+/// Copyright 2012-2026 Olivier Rogier.
 /// See www.ordisoftware.com for more information.
 /// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 /// If a copy of the MPL was not distributed with this file, You can obtain one at
